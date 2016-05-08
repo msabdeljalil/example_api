723f5531f4eea79cc2cbe2b875a41f51f05017e8
Mohammad Abdeljalil <mabdeljalil@enova.com>
2016-05-08 11:57:04 -0500

    Added api/posts route and controller

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,11 @@
+class Api::PostsController < ApplicationController
+  def index
+    render :json => "FF"
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  namespace :api do
+    resources :posts
+  end
 end
